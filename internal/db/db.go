f0e506428eac9f0a4a1c4e7b2063fb9045e53ee1
na3shkw <48012204+na3shkw@users.noreply.github.com>
2026-03-21 22:36:28 +0900

    dbパッケージの各メソッドを実装

--- internal/db/db.go
+++ internal/db/db.go
@@ -1,6 +1,12 @@
 package db
 
-import "database/sql"
+import (
+	"database/sql"
+	"errors"
+	"time"
+
+	_ "modernc.org/sqlite"
+)
 
 func NewClient(dbFile string) (*Client, error) {
 	db, err := sql.Open("sqlite", dbFile)
@@ -24,4 +30,161 @@ func NewClient(dbFile string) (*Client, error) {
 	return &Client{
 		db: db,
 	}, nil
+}
+
+func (c *Client) Create(checksum string, path string) error {
+	_, err := c.db.Exec(
+		`INSERT INTO assets (checksum, path, status, failed_count) VALUES (?, ?, ?, ?)`,
+		checksum, path, "syncing", 0,
+	)
+	return err
+}
+
+func (c *Client) MarkAsSyncing(path string) error {
+	_, err := c.db.Exec(
+		`UPDATE assets SET status = ?, updated_at = ? WHERE path = ?`,
+		"syncing", time.Now(), path,
+	)
+	return err
+}
+
+func (c *Client) MarkAsSuccess(path string, immichId string) error {
+	_, err := c.db.Exec(
+		`UPDATE assets 
+		SET
+			status = ?,
+			immich_id = ?,
+			updated_at = ?
+		WHERE path = ?`,
+		"success", immichId, time.Now(), path,
+	)
+	return err
+}
+
+func (c *Client) MarkAsFailed(path string, reason string) error {
+	_, err := c.db.Exec(
+		`UPDATE assets 
+		SET
+			status = ?,
+			failed_count = failed_count + 1,
+			latest_failed_reason = ?,
+			updated_at = ?
+		WHERE path = ?`,
+		"failed", reason, time.Now(), path,
+	)
+	return err
+}
+
+func (c *Client) SearchByStatus(status string) ([]*Asset, error) {
+	rows, err := c.db.Query(
+		`SELECT
+			id,
+			immich_id,
+			checksum,
+			path,
+			status,
+			failed_count,
+			latest_failed_reason,
+			created_at,
+			updated_at
+		FROM assets WHERE status = ?`,
+		status,
+	)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var assets []*Asset
+	for rows.Next() {
+		asset := Asset{}
+		err = rows.Scan(
+			&asset.ID,
+			&asset.ImmichID,
+			&asset.Checksum,
+			&asset.Path,
+			&asset.Status,
+			&asset.FailedCount,
+			&asset.LatestFailedReason,
+			&asset.CreatedAt,
+			&asset.UpdatedAt,
+		)
+		if err != nil {
+			return nil, err
+		}
+		assets = append(assets, &asset)
+	}
+	return assets, nil
+}
+
+func (c *Client) FindByPath(path string) (*Asset, error) {
+	row := c.db.QueryRow(
+		`SELECT
+			id,
+			immich_id,
+			checksum,
+			path,
+			status,
+			failed_count,
+			latest_failed_reason,
+			created_at,
+			updated_at
+		FROM assets WHERE path = ?`,
+		path,
+	)
+
+	asset := Asset{}
+	err := row.Scan(
+		&asset.ID,
+		&asset.ImmichID,
+		&asset.Checksum,
+		&asset.Path,
+		&asset.Status,
+		&asset.FailedCount,
+		&asset.LatestFailedReason,
+		&asset.CreatedAt,
+		&asset.UpdatedAt,
+	)
+	if errors.Is(err, sql.ErrNoRows) {
+		return nil, nil
+	} else if err != nil {
+		return nil, err
+	}
+	return &asset, nil
+}
+
+func (c *Client) FindByChecksum(checksum string) (*Asset, error) {
+	row := c.db.QueryRow(
+		`SELECT
+			id,
+			immich_id,
+			checksum,
+			path,
+			status,
+			failed_count,
+			latest_failed_reason,
+			created_at,
+			updated_at
+		FROM assets WHERE checksum = ?`,
+		checksum,
+	)
+
+	asset := Asset{}
+	err := row.Scan(
+		&asset.ID,
+		&asset.ImmichID,
+		&asset.Checksum,
+		&asset.Path,
+		&asset.Status,
+		&asset.FailedCount,
+		&asset.LatestFailedReason,
+		&asset.CreatedAt,
+		&asset.UpdatedAt,
+	)
+	if errors.Is(err, sql.ErrNoRows) {
+		return nil, nil
+	} else if err != nil {
+		return nil, err
+	}
+	return &asset, nil
 }
--- internal/db/types.go
+++ internal/db/types.go
@@ -1,10 +1,22 @@
 package db
 
-import "database/sql"
+import (
+	"database/sql"
+	"time"
+)
 
 type Client struct {
 	db *sql.DB
 }
 
 type Asset struct {
+	ID                 int64
+	ImmichID           sql.NullString
+	Checksum           string
+	Path               string
+	Status             string
+	FailedCount        int64
+	LatestFailedReason sql.NullString
+	CreatedAt          time.Time
+	UpdatedAt          time.Time
 }
