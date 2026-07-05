38db07f8cef8264de8a92282290233fd234ae5f4
na3shkw <48012204+na3shkw@users.noreply.github.com>
2026-07-05 19:10:01 +0900

    不要なサンプルコードを削除

--- app.go
+++ app.go
@@ -75,11 +75,6 @@ func (a *App) shutdown(ctx context.Context) {
 	}
 }
 
-// Greet returns a greeting for the given name
-func (a *App) Greet(name string) string {
-	return fmt.Sprintf("Hello %s, It's show time!", name)
-}
-
 func (a *App) LoadConfig() (*config.Config, error) {
 	return config.Load()
 }
